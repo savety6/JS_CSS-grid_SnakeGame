47def0f2b22c3e40c2c5f63c941b9764fa74e912
savety6 <vladimirkucev6@gmail.com>
2021-08-31 15:25:45 +0300

    add phone support

--- input.js
+++ input.js
@@ -1,5 +1,34 @@
 let inputDirection = { x: 0, y:0};
-let lastInputDirection = { x:0, y:0}
+let lastInputDirection = { x:0, y:0};
+let startingX, startingY, movingX, movingY;
+window.addEventListener('touchstart', (evt) => {
+    startingX = evt.touches[0].clientX;
+    startingY = evt.touches[0].clientY;
+})
+window.addEventListener('touchmove', (evt) => {
+    movingX = evt.touches[0].clientX;
+    movingY = evt.touches[0].clientY;
+})
+window.addEventListener('touchend', (evt) => {
+    if(startingX + 100 < movingX) {
+        if (lastInputDirection.x !== 0) return;
+            inputDirection = {x:1, y:0};
+    }
+    else if(startingX - 100 > movingX) {
+        if (lastInputDirection.x !== 0) return;
+            inputDirection = {x:-1, y:0};
+    }
+
+    if (startingY + 100 < movingY) {
+        if (lastInputDirection.y !== 0) return;
+            inputDirection = {x:0, y:1};
+    }
+    else if(startingY - 100 > movingY) {
+        if (lastInputDirection.y !== 0) return;
+            inputDirection = {x:0, y:-1};
+    }
+})
+
 window.addEventListener('keydown', e =>{
     switch(e.key){
         case 'ArrowUp':
